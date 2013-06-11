4547d6fb29fe26f6773b4e24bb3c5de51d261eb9
Manuel <manus@Portatil.(none)>
2013-06-11 12:36:46 +0200

    RC1

--- source/main.c
+++ source/main.c
@@ -32,53 +32,63 @@ _FGS(CODE_PROT_OFF);
 /******************************************************************************/
 /* Global Variable declaration                                                */
 /******************************************************************************/
-#define NTASK_A	taskSensorCO	//Nom Tasca-Funció A
-#define NTASK_B	taskInfoOp		//Nom Tasca-Funció B
-#define	NTASK_C	taskAlarm		//Nom Tasca-Funcio C
+//Tasques activades
+#define NTASK_A	taskH2OFlow	
+#define NTASK_B	taskPump		
+#define	NTASK_C	taskButtons
 
-#define msgPump		OSECBP(1)	//Encesa de la bomba
-#define msgAlarmOn	OSECBP(2)	//Activa l'alarma
-#define msgInfoOp	OSECBP(3)	//Informació al operador
-
+//Configuració de tasques
 #define TASK_A	OSTCBP(1) 	//Tasca A
 #define TASK_B	OSTCBP(2) 	//Tasca B
 #define TASK_C	OSTCBP(3) 	//Tasca C
 
-#define PRIO_A	10		 	//Prioritat tasca A
-#define PRIO_B	10		 	//Prioritat tasca B
-#define PRIO_C	10		 	//Prioritat tasca C
+#define PRIO_A	10	//Prioritat tasca A
+#define PRIO_B	10	//Prioritat tasca B
+#define PRIO_C	10	//Prioritat tasca C
 
+//Missatges
+#define msgPump		OSECBP(1)	//Encesa de la bomba
+#define msgAlarmOn	OSECBP(2)	//Activa l'alarma
+#define msgInfoOp	OSECBP(3)	//Informació al operador
+
+//Botons
 #define SENSOR_LOW	Buttons.SW1	//Sensor baix
 #define SENSOR_HIGH	Buttons.SW2	//Sensor alt
 #define H2O_FLOW	Buttons.SW3	//Fluxe d'aigua
 #define BUT_ACT		Buttons.SW4	//Botó activació bomba
 #define BUT_DES		Buttons.SW5	//Botó desactivació bomba
 
-#define TIMER_PERIOD 0x2300//(100ms)
-#define TMR0_RELOAD 156
+//Configuració del Timer
+#define TIMER_PERIOD 0x250	//(100ms)
+#define TMR0_RELOAD  156
 
+//Límits d'activació dels sensors
+#define	L_CH4	500	//Sensor de CH4
+#define L_CO	700	//Sensor de CO
+#define L_FW	200	//Sensor de fluxe d'aire
+
+//Senyals dels missatges
 char S_ON = 'O';	//Senyal de encesa del motor
 char S_OFF = 'F';	//Senyal d'aturada del motor
 char S_CH4 = '4';	//Senyal del sensor de CH4
 char S_CO = 'C';	//Senyal del sensor de CO
 char S_AIR = 'A';	//Senyal de fluxe d'aire
-char E_BOMB = 'B';	//Senyal d'error de fluxe d'aigua
+char S_BOMB = 'B';	//Senyal d'error de fluxe d'aigua
 
-char motorOn = 'F';		//Bomba d'aigua. Desactivada inicialment
-char lvlCH4 = 'F';	//Estat del metà. Inicialment F
+//Estats del sistema
+char motorOn = 'F';	//Bomba d'aigua. Desactivada inicialment
+char lvlCH4 = 'F';	//Estat del metà.
 
 /******************************************************************************/
 /* Interrupts                                                                 */
 /******************************************************************************/
 void __attribute__((interrupt, auto_psv)) _T1Interrupt(){
-     if (IEC0bits.T1IE && IFS0bits.T1IF) {
-      WriteTimer1(0x0000);
-      IFS0bits.T1IF=0;
-      TMR1 -= TMR0_RELOAD;
-      OSTimer();
-      
-    }
-    OS_Yield();
+	if (IEC0bits.T1IE && IFS0bits.T1IF) {
+		WriteTimer1(0x0000);            // Reset time counter
+		OSTimer();
+		IFS0bits.T1IF = 0;
+		TMR1 -= TMR0_RELOAD;		
+	}
 }
 
 /******************************************************************************/
@@ -95,7 +105,7 @@ void taskSensorLvl(void){
 		if(SENSOR_HIGH == 1){
 			OSSignalMsg(msgPump, (OStypeMsgP) &S_ON);
 		}
-		OS_Yield();
+		OS_Delay(2);
 	}
 }
 
@@ -109,7 +119,7 @@ void taskButtons(void){
 		if(BUT_DES == 1){
 			OSSignalMsg(msgPump, (OStypeMsgP) &S_OFF);
 		}	
-		OS_Yield();
+		OS_Delay(2);
 	}
 }
 
@@ -119,17 +129,17 @@ void taskSensorCH4(void){
 	
 	while(1){		
 		uiMeasure = AnalogAcquireR1();
-		if(uiMeasure > 500){
+		if(uiMeasure > L_CH4){
 			lvlCH4 = 'T';
 			OSSignalMsg(msgAlarmOn, (OStypeMsgP) &S_CH4);
 			OSSignalMsg(msgInfoOp, (OStypeMsgP) &S_CH4);
 			OSSignalMsg(msgPump, (OStypeMsgP) &S_OFF);
 		}
-		if(uiMeasure <= 500){
+		if(uiMeasure <= L_CH4){
 			lvlCH4 = 'F';
 		}
 		
-		OS_Yield();
+		OS_Delay(1);
 	}
 }
 
@@ -139,11 +149,11 @@ void taskSensorCO(void){
 	
 	while(1){		
 		uiMeasure = AnalogAcquireR1();
-		if(uiMeasure > 700){
+		if(uiMeasure > L_CO){
 			OSSignalMsg(msgAlarmOn, (OStypeMsgP) &S_CO);
 			OSSignalMsg(msgInfoOp, (OStypeMsgP) &S_CO);
 		}		
-		OS_Yield();
+		OS_Delay(1);
 	}
 }
 
@@ -153,11 +163,11 @@ void taskSensorAirFlow(void){
 	
 	while(1){		
 		uiMeasure = AnalogAcquireR1();
-		if(uiMeasure < 200){
+		if(uiMeasure < L_FW){
 			OSSignalMsg(msgAlarmOn, (OStypeMsgP) &S_AIR);
 			OSSignalMsg(msgInfoOp, (OStypeMsgP) &S_AIR);
 		}		
-		OS_Yield();
+		OS_Delay(1);
 	}
 }
 
@@ -165,13 +175,14 @@ void taskH2OFlow(void){
 	/* Tasca que controla el correcte funcionament de la bomba */
 	while(1){
 		PushButtonRead();
-		if((motorOn == 'T') && (H2O_FLOW == 0)){
-			OSSignalMsg(msgInfoOp, (OStypeMsgP) &E_BOMB);
+		if(((motorOn == 'T') && (H2O_FLOW == 0)) ||
+			((motorOn == 'F') && (H2O_FLOW == 1))){
+			OSSignalMsg(msgInfoOp, (OStypeMsgP) &S_BOMB);
+			LED5 = 1;
+		}else{
+			LED5 = 0;
 		}
-		if((motorOn == 'F') && (H2O_FLOW == 1)){
-			OSSignalMsg(msgInfoOp, (OStypeMsgP) &E_BOMB);
-		}	
-		OS_Yield();
+		OS_Delay(9);
 	}
 }
 
@@ -211,7 +222,7 @@ void taskInfoOp(void){
 		if(*(char *)msgP == S_AIR){
 			LCDWriteString("Fluxe d'aire    ");
 		}
-		if(*(char *)msgP == E_BOMB){
+		if(*(char *)msgP == S_BOMB){
 			LCDWriteString("Error de bomba  ");
 		}
 	}
@@ -235,8 +246,8 @@ void taskPump(void){
 	}
 }
 
-void TimerConfig(){
-    OpenTimer1(T1_ON &              // Start 16-bit Timer
+void Micro_Init(){
+	OpenTimer1(T1_ON &              // Start 16-bit Timer
                T1_IDLE_CON &        // Continue operation on idle
                T1_GATE_OFF &        // Timer gate accumulation disabled
                T1_PS_1_256 &        // Prescale 1:256
@@ -244,17 +255,17 @@ void TimerConfig(){
                T1_SOURCE_INT,       // Internal osc (FCY)
                TIMER_PERIOD);       // Desired Period
 
-    ConfigIntTimer1(T1_INT_PRIOR_4 & // Interrupt Priority 4 (7 Highest)
-                    T1_INT_ON);     // Interrupt disabled
+    ConfigIntTimer1(T1_INT_PRIOR_7 & // Interrupt Priority 4 (7 Highest)
+                    T1_INT_ON);      // Interrupt enabled
 }
 
 int main( void ){
 	U1MODE = 0; //Clear UART config - to avoid problems with bootloader
+	Micro_Init();
 	IOConfig();
 	AnalogConfig();
 	LCDConfig(0);
 	OSInit();
-	//TimerConfig();
 	
 	LCDWriteString((char*)"Control de mina");
 	
